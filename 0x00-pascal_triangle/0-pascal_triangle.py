7b51071647acad5b3d1206cb8cd02f358e2dc0b3
harkinder-dark <victorvaddely@gmail.com>
2023-04-18 22:45:33 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+def fac(n):
+    """
+    factoriel de n
+    """
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+def cmb(k, n):
+    """
+    combinaison
+    """
+    return fac(n) // (fac(n-k)*fac(k))
+
+def pascal_triangle(n):
+    """
+    n: integer
+     __return__
+    """
+    res = []
+    if n > 0:
+        for i in range(n):
+            res.append([cmb(k, i) for k in range(i + 1)])
+    return res
