56374d15ff4a62f60c76197418e35414ba05f21d
harkinder-dark <victorvaddely@gmail.com>
2023-04-25 19:29:06 +0000

    task 0 finish

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Vous avez n nombre de boîtes verrouillées devant vous.
+Chaque case est numérotée séquentiellement de 0 à n - 1 et
+chaque case peut contenir les clés des autres cases.
+
+Write a method that determines if all the boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """
+    boxes : parameters
+    _return_ : boolean
+    """
+    res = [0]
+    if len(boxes[0]) == 0:
+        return False
+    for i in res:
+        for j in boxes[i]:
+            if j <= len(boxes) - 1:
+                res.append(j)
+    if len(res) >= len(boxes):
+        return True
+    else:
+        return False
---
+++ README.md
@@ -0,0 +1,1 @@
+alx-intervien project for backend
