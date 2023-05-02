8cecfaee262be7c6dd6ef2b60352ab6611340537
harkinder-dark <victorvaddely@gmail.com>
2023-05-02 15:09:59 +0000

    alx interview 3

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single
+character H. Your text editor can
+execute only two operations in
+this file:
+"""
+def minOperations(n):
+    """
+    n : int
+    _return_ :
+    """
+    if n <= 1:
+        return 0
