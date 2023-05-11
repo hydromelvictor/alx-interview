397e12f55df2954f3e2eeda1708d1589fd15dedf
harkinder-dark <victorvaddely@gmail.com>
2023-05-11 10:06:42 +0000

    stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,14 +15,17 @@ def parsing():
     date_re = r"(\d+/\d+/\d+)"
     status_re = r"(200|301|400|401|403|404|405|500)"
     size_re = r"(\d+)$"
+    format = ip_re + ' - ' + '[' + date_re + ']' + "GET /projects/260 HTTP/1.1" + ' ' + status_re + ' ' + size_re
     i = 0
     size = 0
     status = [200, 301, 400, 401, 403, 404, 405, 500]
     code = {i: 0 for i in status}
 
-    with open(sys.stdin) as file:
-        line = file.readline()
+    for line in sys.stdin:
         i += 1
+
+        if re.search(format, line) != line:
+            continue
 
         size += int(line[-1])
         if line[-2] in status:
