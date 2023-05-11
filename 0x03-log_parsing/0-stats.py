0b9751858aa816d57dd74ad3ad09691f386b8ef6
harkinder-dark <victorvaddely@gmail.com>
2023-05-11 10:20:42 +0000

    stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,11 +10,6 @@ def parsing():
     """
     return : none
     """
-    lines = []
-    ip_re = r"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])"
-    date_re = r"(\d+/\d+/\d+)"
-    status_re = r"(200|301|400|401|403|404|405|500)"
-    size_re = r"(\d+)$"
     i = 0
     size = 0
     status = [200, 301, 400, 401, 403, 404, 405, 500]
@@ -23,11 +18,12 @@ def parsing():
         for url in sys.stdin:
             line = url.split()
             i += 1
+
             try:
                 size += int(line[-1])
             except Exception:
                 pass
-            
+
             try:
                 stat = line[-2]
                 if stat in status:
@@ -39,6 +35,7 @@ def parsing():
                 print("File size: {}".format(size))
                 for key, val in code.items():
                     print(f"{key}: {val}")
+
     except KeyboardInterrupt:
         print("File size: {}".format(size))
         for key, val in code.items():
