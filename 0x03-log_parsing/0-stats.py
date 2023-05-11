b62a5bbd787de6ed615706f28ffddc10a8bb43ed
harkinder-dark <victorvaddely@gmail.com>
2023-05-11 10:17:46 +0000

    stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,18 +19,30 @@ def parsing():
     size = 0
     status = [200, 301, 400, 401, 403, 404, 405, 500]
     code = {i: 0 for i in status}
+    try:
+        for url in sys.stdin:
+            line = url.split()
+            i += 1
+            try:
+                size += int(line[-1])
+            except Exception:
+                pass
+            
+            try:
+                stat = line[-2]
+                if stat in status:
+                    code[stat] += 1
+            except Exception:
+                pass
 
-    for line in sys.stdin:
-        i += 1
-
-        size += int(line[-1])
-        if line[-2] in status:
-            code[line[-2]] += 1
-
-        if i % 10 == 0:
-            print("File size: {}".format(size))
-            for key, val in code.items():
-                print(f"{key}: {val}")
+            if i % 10 == 0:
+                print("File size: {}".format(size))
+                for key, val in code.items():
+                    print(f"{key}: {val}")
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for key, val in code.items():
+            print(f"{key}: {val}")
 
 if __name__ == '__main__':
     parsing()
