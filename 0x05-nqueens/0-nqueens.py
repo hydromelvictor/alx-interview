9ee2a0c3fbfce5d60e3d149f896b671553610872
harkinder-dark <victorvaddely@gmail.com>
2023-05-24 10:04:38 +0000

    add new directory 0x05-nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""The N queens puzzle is the challenge of placing N
+non-attacking queens on an NxN chessboard
+"""
+import sys
+
+
+def nqueens():
+    """n queens resolve"""
+    if len(sys.argv) < 2:
+        print('Usage: nqueens N, followed by a new line')
+        exit(1)
+    n = sys.argv[1]
+    try:
+        n = int(n)
+    except Exception:
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+    
+    if n % 2 != 0:
+        print([[]])
+
+    for k in range(n):
+        line = []
+        for l in range(n):
+            if k != l and k + l != n - 1:
+                line.append([i, j])
+            print(line)
+
+
+if __name__ == '__main__':
+    nqueens()
