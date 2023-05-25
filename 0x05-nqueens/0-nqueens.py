4f5825fb914d904fcd384756d0d34efd207158bf
harkinder-dark <victorvaddely@gmail.com>
2023-05-25 16:18:39 +0000

    update nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,77 +5,83 @@ non-attacking queens on an NxN chessboard
 import sys
 
 
-def diagRigthBottom(point, n):
+def diagLeftBottom(pos, n):
     line = []
-    i, j = point
-    while (i < (n - 1) or j < (n - 1)):
-        line.extend([i, j])
-        i += 1
-        j += 1
+    p, q = pos
+    while p < (n - 1) or q > 0:
+        line.append(p)
+        line.append(q)
+        p += 1
+        q -= 1
     return line
 
 
-def diagRigthTop(point, n):
+def diagLeftTop(pos, n):
     line = []
-    i, j = point
-    while (i > 0 and j < (n - 1)):
-        line.extend([i, j])
-        i -= 1
-        j += 1
+    p, q = pos
+    while p > 0 or q > 0:
+        line.append(p)
+        line.append(q)
+        p -= 1
+        q -= 1
     return line
 
 
-def diagLeftBottom(point, n):
+def diagRigthBottom(pos, n):
     line = []
-    i, j = point
-    while (i < (n - 1) or j > 0):
-        line.extend([i, j])
-        i += 1
-        j -= 1
+    p, q = pos
+    while p < (n - 1) or q < (n - 1):
+        line.append(p)
+        line.append(q)
+        p += 1
+        q += 1
     return line
 
 
-def diagLeftTop(point, n):
+def diagRigthTop(pos, n):
     line = []
-    i, j = point
-    while (i > 0 or j > 0):
-        line.extend([i, j])
-        i -= 1
-        j -= 1
+    p, q = pos
+    while p > 0 or q < (n - 1):
+        line.append(p)
+        line.append(q)
+        p -= 1
+        q += 1
     return line
 
 
 def nqueens():
-    """n queens resolve"""
-    if len(sys.argv) < 2:
-        print('Usage: nqueens N, followed by a new line')
+    """n queens algorithms"""
+    str = sys.argv
+    if len(str) < 2:
+        print('Usage: nqueens N')
         exit(1)
-    n = sys.argv[1]
+    n = str[1]
     try:
         n = int(n)
     except Exception:
+        print('N must be a number')
         exit(1)
-
     if n < 4:
         print('N must be at least 4')
         exit(1)
-    
-    echiquier = [[i, j] for i in range(n) for j in range(n)]
-    bad = []
-    s = 0
-    while(s < n):
+    # toutes les positions de l'echiquier
+    ech = [[i, j] for i in range(n) for j in range(n)]
+    tr = 0
+    # boucle parcours n fois
+    while tr < n:
         line = []
-        for k in echiquier[s:n]:
-            i, j = k
-            if k not in line and (i not in bad or j not in bad):
-                line.append(k)
-                bad.extend(diagLeftBottom(k, n))
-                bad.extend(diagLeftTop(k, n))
-                bad.extend(diagRigthBottom(k, n))
-                bad.extend(diagRigthTop(k, n))
-        s += 1
+        bad = []
+        for pos in ech[tr:]:
+            p, q = pos
+            if pos not in line and p not in bad and q not in bas:
+                line.append(pos)
+                bad.extend(diagLeftTop(pos, n))
+                bad.extend(diagLeftBottom(pos, n))
+                bad.extend(diagRigthTop(pos, n))
+                bad.extend(diagRigthBottom(pos, n))
         if len(line) == n:
             print(line)
+        tr += 1
 
 
 if __name__ == '__main__':
