da43409d7462d14a360055685688c20e6aaad96c
harkinder-dark <victorvaddely@gmail.com>
2023-06-03 04:41:18 +0000

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,27 +2,14 @@
 const request = require('request');
 const { argv } = require('process');
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
-function validStatus (link) {
-  return new Promise(function (resolve, reject) {
-    request(link, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
+url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(function (elt) {
+      const name = JSON.parse(characters).name;
+      console.log(name);
     });
-  });
-}
-
-async function main () {
-  const film = await validStatus(url + argv[2]);
-  const chars = JSON.parse(film).characters;
-  chars.forEach(async function (elt) {
-    const char = await validStatus(elt);
-    const name = JSON.parse(char).name
-    console.log(name);
-  });
-}
-
-main();
+  }
+});
