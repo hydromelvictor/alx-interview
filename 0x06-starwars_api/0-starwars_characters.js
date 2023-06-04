52fe4f959b129bca2b35d4e8f03f4ccabd0a9b78
harkinder-dark <victorvaddely@gmail.com>
2023-06-04 15:37:20 +0000

    starwars_characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const { argv } = require('process');
 
-url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+const movie = proccess.argv[2];
+const baseUrl = 'https://swapi-api.alx-tools.com/';
+const endpoint = `${baseUrl}/films/${movie}`;
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach(function (elt) {
-      const name = JSON.parse(characters).name;
-      console.log(name);
+request(endpoint, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+
+    characters.forEach((charUrl) => {
+      request(charUrl, (error, reponse, body) => {
+        if (!error && reponse.statusCode == 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
     });
   }
 });
