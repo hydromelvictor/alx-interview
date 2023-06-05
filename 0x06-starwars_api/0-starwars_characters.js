0da4b4b34da58e14cab8e2fad536d30084252ee4
harkinder-dark <victorvaddely@gmail.com>
2023-06-05 11:02:27 +0000

    add process

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,13 +7,13 @@ const baseUrl = 'https://swapi-api.hbtn.io/api/';
 const endpoint = `${baseUrl}/films/${movie}`;
 
 request(endpoint, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     const film = JSON.parse(body);
     const characters = film.characters;
 
     characters.forEach((charUrl) => {
       request(charUrl, (error, reponse, body) => {
-        if (!error && reponse.statusCode == 200) {
+        if (!error && reponse.statusCode === 200) {
           const character = JSON.parse(body);
           console.log(character.name);
         }
